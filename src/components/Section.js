847242db73a4240b91d6a5872e302b542b24cb7f
Tomas Vink <tomasvink@gmail.com>
2021-06-09 15:27:55 +0200

    better padding

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,16 +2,12 @@ import React from 'react'
 
 const Section = ({ title, children }) => (
   <section id='scroll'>
-    <div className='container px-5'>
+    <div className='container px-4 my-5'>
       <div className='row gx-5'>
         <div className='col-lg-4 order-lg-1'>
-          <div className='p-5'>
-            <h2 className='display-5'>{title}</h2>
-          </div>
+          <h2 className='display-5'>{title}</h2>
         </div>
-        <div className='col-lg-8 order-lg-2'>
-          <div className='p-5'>{children}</div>
-        </div>
+        <div className='col-lg-8 order-lg-2'>{children}</div>
       </div>
     </div>
   </section>
